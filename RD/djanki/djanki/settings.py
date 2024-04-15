8a4926ff4acc6a99896677bc635b5863f3a1d667
qiuqihua <2395669615@qq.com>
2024-04-15 08:58:16 +0800

    登录、知识点、试题添加、学习情况

--- RD/djanki/djanki/settings.py
+++ RD/djanki/djanki/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'study_sys',  # 你的数据库名字
         'USER': 'postgres',  # 你的PostgreSQL用户名
         'PASSWORD': 'Qq123123',  # 你的PostgreSQL密码
-        'HOST': '10.115.56.27',  # 你的数据库主机，本地通常是localhost
+        'HOST': '10.115.58.248',  # 你的数据库主机，本地通常是localhost
         'PORT': '5432',  # 你的PostgreSQL端口，5432是默认端口
     }
 }
